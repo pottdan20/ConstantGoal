776668dcbeda011634aa63a50a6fd6371d4375f1
pottdan20 <potterd123@gmail.com>
2025-11-16 11:45:02 -0800

    Initial Commit

---
+++ Constant Goal/Constant_GoalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Constant_GoalApp.swift
+//  Constant Goal
+//
+//  Created by Dane on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Constant_GoalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Constant Goal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Constant Goal
+//
+//  Created by Dane on 11/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
